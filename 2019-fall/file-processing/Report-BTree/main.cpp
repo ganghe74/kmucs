99463d2e7f3e63d2cd0ed882ec8927738f2a08a5
ganghe74 <ganghe74@kookmin.ac.kr>
2019-11-25 19:54:18 +0900

    B-Tree Delete

--- 2019-fall/file-processing/Report-BTree/main.cpp
+++ 2019-fall/file-processing/Report-BTree/main.cpp
@@ -4,6 +4,8 @@
 #include <algorithm>
 using namespace std;
 
+enum FLAG {LEFT, RIGHT, MERGE};
+
 class BTreeNode {
 public:
     BTreeNode(int key) {
@@ -15,7 +17,7 @@ public:
     ~BTreeNode() {
         for (auto child : childs) delete child;
     }
-    void insertKey(int newKey, BTreeNode *newChild=nullptr) {
+    void insertKey(int newKey, BTreeNode *newChild=nullptr, int newChildPos=RIGHT) {
         keys.push_back(newKey);
         childs.push_back(newChild);
         int i = ++n - 1;
@@ -24,7 +26,20 @@ public:
             swap(childs[i], childs[i+1]);
             i--;
         }
+        if (newChildPos == LEFT) swap(childs[i], childs[i+1]);
     }
+
+    bool deleteKey(int oldKey, int oldChildPos=RIGHT) {
+        int i = 0;
+        while (i < n && oldKey > keys[i]) i++;
+        if (keys[i] != oldKey) return false;
+        keys.erase(keys.begin() + i);
+        if (oldChildPos == RIGHT) i++;
+        childs.erase(childs.begin() + i);
+        n--;
+        return true;
+    }
+
     int n = 0;
     vector<int> keys;
     vector<BTreeNode*> childs;
@@ -73,7 +88,88 @@ bool insertBT(BTreeNode *&T, int newKey) {
 }
 
 bool deleteBT(BTreeNode *&T, int oldKey) {
-    // TODO:
+    stack<BTreeNode*> st;
+    BTreeNode *x = T;
+    while (x != nullptr) {
+        int i = 0;
+        while (i < x->n && oldKey > x->keys[i]) i++;
+        if (i < x->n && oldKey == x->keys[i]) break;
+        st.push(x);
+        x = x->childs[i];
+    }
+    if (x == nullptr) return false; // Search Fail
+    if (x->childs[0] != nullptr) { // if x is Internal Node
+        BTreeNode *internalNode = x;
+        st.push(x);
+        x = x->childs[find(x->keys.begin(), x->keys.end(), oldKey) - x->keys.begin()+1];
+        while (x != nullptr) {
+            st.push(x);
+            x = x->childs[0];
+        }
+        x = st.top(); st.pop();
+        *find(internalNode->keys.begin(), internalNode->keys.end(), oldKey) = x->keys[0];
+        x->keys[0] = oldKey;
+    }
+    x->deleteKey(oldKey);
+    BTreeNode *y, *leftSibling, *rightSibling;
+    while (x->n < (BTreeNode::M+1)/2-1 && !st.empty()) { // While Underflow
+        y = st.top(); st.pop();
+        int idx = find(y->childs.begin(), y->childs.end(), x) - y->childs.begin();
+        if (x == y->childs[0]) leftSibling = nullptr;
+        else leftSibling = y->childs[idx-1];
+        if (x == y->childs.back()) rightSibling = nullptr;
+        else rightSibling = y->childs[idx+1];
+        int flag;
+        if (leftSibling == nullptr || rightSibling == nullptr) {
+            if (leftSibling == nullptr) {
+                if (rightSibling->n > (BTreeNode::M-1)/2) flag = RIGHT;
+                else flag = MERGE;
+            }
+            else {
+                if (leftSibling->n > (BTreeNode::M-1)/2) flag = LEFT;
+                else flag = MERGE;
+            }
+        }
+        else {
+            if (leftSibling->n >= rightSibling->n && leftSibling->n > (BTreeNode::M-1)/2) flag = LEFT;
+            else if (rightSibling->n > leftSibling->n) flag = RIGHT;
+            else flag = MERGE;
+        }
+        if (flag == LEFT) { // Get key from left sibling
+            x->insertKey(y->keys[idx-1], leftSibling->childs.back(), LEFT);
+            y->keys[idx-1] = leftSibling->keys.back();
+            leftSibling->deleteKey(leftSibling->keys.back());
+        }
+        else if (flag == RIGHT) { // Get key from right sibling
+            x->insertKey(y->keys[idx], rightSibling->childs[0], RIGHT);
+            y->keys[idx] = rightSibling->keys[0];
+            rightSibling->deleteKey(rightSibling->keys[0], LEFT);
+        }
+        else { // Merge
+            if (leftSibling == nullptr) {
+                idx++;
+                leftSibling = x;
+                x = rightSibling;
+            }
+            leftSibling->keys.push_back(y->keys[idx-1]);
+            leftSibling->n++;
+            y->deleteKey(y->keys[idx-1], RIGHT);
+            leftSibling->keys.insert(leftSibling->keys.end(), x->keys.begin(), x->keys.end());
+            leftSibling->childs.insert(leftSibling->childs.end(), x->childs.begin(), x->childs.end());
+            leftSibling->n += x->n;
+            x->childs.clear();
+            delete x;
+        }
+        x = y;
+    }
+    if (T->n == 0) {
+        y = T;
+        T = T->childs[0];
+        y->childs.clear();
+        delete y;
+    }
+
+    return true;
 }
 
 void inorderBT(BTreeNode *T) {
@@ -100,13 +196,25 @@ int main() {
         53, 57, 52, 56, 83, 81, 82, 84, 75, 89
     };
 
+    int deleteKeys[] = {
+        66, 10, 22, 30, 44, 55, 50, 60, 100, 28,
+        18, 9, 5, 17, 6, 3, 1, 4, 2, 7,
+        8, 73, 12, 13, 14, 16, 15, 25, 24, 28,
+        40, 11, 77, 33, 20, 90, 99, 70, 88, 80,
+        45, 49, 42, 43, 41, 47, 48, 46, 63, 68,
+        53, 57, 52, 56, 83, 81, 82, 84, 75, 89,
+        61, 62, 64, 69, 67, 65, 54, 59, 58, 51
+    };
+
     BTreeNode *root = nullptr;
 
     BTreeNode::M = 3;
     cout << "M = 3" << endl;
-    for (int key : insertKeys) {
-        cout << "Insert " << key << '\t';
-        insertBT(root, key);
+    for (int key : insertKeys) insertBT(root, key);
+
+    for (int key : deleteKeys) {
+        cout << "Delete " << key << '\t';
+        deleteBT(root, key);
         inorderBT(root);
         cout << endl;
     }
@@ -117,11 +225,13 @@ int main() {
     cout << "==================" << endl;
     BTreeNode::M = 4;
     cout << "M = 4" << endl;
-    for (int key : insertKeys) {
-        cout << "Insert " << key << '\t';
-        insertBT(root, key);
+    for (int key : insertKeys) insertBT(root, key);
+
+    for (int key : deleteKeys) {
+        cout << "Delete " << key << '\t';
+        deleteBT(root, key);
         inorderBT(root);
-        cout << endl; 
+        cout << endl;
     }
 
     delete root;
